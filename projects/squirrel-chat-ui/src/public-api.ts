55e1556ef0de34f71a82c7ce1fc390a4d21c7a30
Hannes Rüger <hannesrueger@gmx.de>
2020-06-10 16:20:37 +0200

    removed useless service

--- projects/squirrel-chat-ui/src/public-api.ts
+++ projects/squirrel-chat-ui/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of squirrel-chat-ui
  */
 
-export * from './lib/squirrel-chat-ui.service';
 export * from './lib/squirrel-chat-ui.component';
 export * from './lib/squirrel-chat-ui.module';
