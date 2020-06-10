e3fb17fc26404152df4df2e84cd3bd8b73986e22
Hannes Rüger <hannesrueger@gmx.de>
2020-06-10 16:59:40 +0200

    added basic layout

--- projects/squirrel-chat-ui/src/lib/squirrel-chat-ui.component.ts
+++ projects/squirrel-chat-ui/src/lib/squirrel-chat-ui.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'squirrel-chat-ui',
   templateUrl: "./squirrel-chat-ui.component.html",
-  styles: [
-  ]
+  styleUrls: ["./squirrel-chat-ui.component.scss"],
 })
 export class SquirrelChatUiComponent implements OnInit {
 
