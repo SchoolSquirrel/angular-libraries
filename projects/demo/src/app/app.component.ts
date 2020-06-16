64dc6b551a40bd31588c6610f513ca54c4e312d5
Hannes Rüger <hannesrueger@gmx.de>
2020-06-16 14:14:26 +0200

    messages are now an input

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Message, MessageStatus } from "squirrel-chat-ui";
 
 @Component({
     selector: "app-root",
@@ -6,5 +7,78 @@ import { Component } from "@angular/core";
     styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "demo";
+  private user1 = { id: 1, name: "Güther" };
+  private user2 = { id: 2, name: "Tom" };
+  public messages: Message[] = [
+      {
+          id: 0,
+          fromMe: false,
+          text: "Hallo!",
+          sender: this.user1,
+      },
+      {
+          id: 1,
+          fromMe: true,
+          text: "Hallo!",
+          sender: this.user2,
+      },
+      {
+          id: 2,
+          fromMe: false,
+          text: "Wie geht's?",
+          sender: this.user1,
+      },
+      {
+          id: 3,
+          fromMe: false,
+          text: "Hallo",
+          sender: this.user1,
+      },
+      {
+          id: 4,
+          fromMe: false,
+          text: "...",
+          sender: this.user1,
+      },
+      {
+          id: 5,
+          fromMe: true,
+          text: "Gut",
+          sender: this.user2,
+      },
+      {
+          id: 6,
+          fromMe: false,
+          text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+          sender: this.user1,
+      },
+      {
+          id: 7,
+          fromMe: true,
+          text: "Seen",
+          sender: this.user2,
+          status: MessageStatus.Seen,
+      },
+      {
+          id: 8,
+          fromMe: true,
+          text: "Delivered",
+          sender: this.user2,
+          status: MessageStatus.Delivered,
+      },
+      {
+          id: 9,
+          fromMe: true,
+          text: "Sent",
+          sender: this.user2,
+          status: MessageStatus.Sent,
+      },
+      {
+          id: 10,
+          fromMe: true,
+          text: "Waiting. Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines?",
+          sender: this.user2,
+          status: MessageStatus.Waiting,
+      },
+  ];
 }
--- projects/squirrel-chat-ui/src/lib/squirrel-chat-ui.component.ts
+++ projects/squirrel-chat-ui/src/lib/squirrel-chat-ui.component.ts
@@ -1,5 +1,5 @@
 import {
-    Component, OnInit, ViewChild, ElementRef,
+    Component, OnInit, ViewChild, ElementRef, Input,
 } from "@angular/core";
 import { Message } from "./Message";
 import { MessageStatus } from "./MessageStatus";
@@ -14,81 +14,9 @@ export class SquirrelChatUiComponent implements OnInit {
     public message = "";
     public showEmojiPicker = false;
     public showAttachmentsCard = false;
-    private user1 = { id: 1, name: "Güther" };
-    private user2 = { id: 2, name: "Tom" };
     private disableScrollDown = false;
-    public messages: Message[] = [
-        {
-            id: 0,
-            fromMe: false,
-            text: "Hallo!",
-            sender: this.user1,
-        },
-        {
-            id: 1,
-            fromMe: true,
-            text: "Hallo!",
-            sender: this.user2,
-        },
-        {
-            id: 2,
-            fromMe: false,
-            text: "Wie geht's?",
-            sender: this.user1,
-        },
-        {
-            id: 3,
-            fromMe: false,
-            text: "Hallo",
-            sender: this.user1,
-        },
-        {
-            id: 4,
-            fromMe: false,
-            text: "...",
-            sender: this.user1,
-        },
-        {
-            id: 5,
-            fromMe: true,
-            text: "Gut",
-            sender: this.user2,
-        },
-        {
-            id: 6,
-            fromMe: false,
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-            sender: this.user1,
-        },
-        {
-            id: 7,
-            fromMe: true,
-            text: "Seen",
-            sender: this.user2,
-            status: MessageStatus.Seen,
-        },
-        {
-            id: 8,
-            fromMe: true,
-            text: "Delivered",
-            sender: this.user2,
-            status: MessageStatus.Delivered,
-        },
-        {
-            id: 9,
-            fromMe: true,
-            text: "Sent",
-            sender: this.user2,
-            status: MessageStatus.Sent,
-        },
-        {
-            id: 10,
-            fromMe: true,
-            text: "Waiting. Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines? Also a very long message... Maybe multiple lines?",
-            sender: this.user2,
-            status: MessageStatus.Waiting,
-        },
-    ];
+    @Input() public messages: Message[] = [];
+
     constructor() {
         this.scrollToBottom();
     }
