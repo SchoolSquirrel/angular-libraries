c3f28e6a206ae7cd3d032871d80454bec786c344
Hannes Rüger <hannesrueger@gmx.de>
2020-10-10 16:21:14 +0200

    demo now displays multiple packages

---
+++ projects/demo/src/app/_components/forms-demo/forms-demo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-forms-demo",
+    templateUrl: "./forms-demo.component.html",
+    styleUrls: ["./forms-demo.component.scss"],
+})
+export class FormsDemoComponent {
+}
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
-// used for the demo
 import { ChatUiModule } from "@schoolsquirrel/chat-ui";
-
+import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
+import { ChatUiDemoComponent } from "./_components/chat-ui-demo/chat-ui-demo.component";
+import { FormsDemoComponent } from "./_components/forms-demo/forms-demo.component";
+import { IndexComponent } from "./_components/index/index.component";
 
 @NgModule({
     declarations: [
         AppComponent,
+        ChatUiDemoComponent,
+        FormsDemoComponent,
+        IndexComponent,
     ],
     imports: [
-        BrowserModule,
         ChatUiModule,
         BrowserModule,
+        RouterModule.forRoot([
+            { path: "chat-ui", component: ChatUiDemoComponent },
+            { path: "forms", component: FormsDemoComponent },
+            { path: "", component: IndexComponent },
+        ]),
     ],
     providers: [],
     bootstrap: [AppComponent],
