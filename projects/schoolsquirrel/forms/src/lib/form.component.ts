d326d2e194aac4cf1c9a495e829bbbb298c87429
Hannes Rüger <hannesrueger@gmx.de>
2020-10-11 10:52:56 +0200

    (forms) added support for `textarea`

--- projects/schoolsquirrel/forms/src/lib/form.component.ts
+++ projects/schoolsquirrel/forms/src/lib/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { Field } from "./Field";
 import { Form } from "./Form";
 
 @Component({
@@ -10,4 +11,12 @@ import { Form } from "./Form";
 })
 export class FormComponent {
     @Input() public form: Form = {} as Form;
+
+    public isInputTag(field: Field): boolean {
+        return ["number", "text", "date"].includes(field.type);
+    }
+
+    public isTextareaTag(field: Field): boolean {
+        return field.type == "textarea";
+    }
 }
