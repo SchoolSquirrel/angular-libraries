a3d0ca6487a358f9bf3a6cadbc4c3de7d200d886
Hannes Rüger <hannesrueger@gmx.de>
2020-10-11 17:09:14 +0200

    (demo) show filemanager

---
+++ projects/demo/src/app/_components/file-manager-demo/file-manager-demo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-file-manager-demo",
+    templateUrl: "./file-manager-demo.component.html",
+    styleUrls: ["./file-manager-demo.component.scss"],
+})
+export class FileManagerDemoComponent {
+}
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { ChatUiModule } from "@schoolsquirrel/chat-ui";
 import { RouterModule } from "@angular/router";
 // import { SchoolSquirrelFormsModule } from "@schoolsquirrel/forms";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ChatUiModule } from "../../../schoolsquirrel/chat-ui/src/public-api";
 import { SchoolSquirrelFormsModule } from "../../../schoolsquirrel/forms/src/public-api";
+import { FileManagerModule } from "../../../schoolsquirrel/filemanager/src/public-api";
 import { AppComponent } from "./app.component";
 import { ChatUiDemoComponent } from "./_components/chat-ui-demo/chat-ui-demo.component";
 import { FormsDemoComponent } from "./_components/forms-demo/forms-demo.component";
+import { FileManagerDemoComponent } from "./_components/file-manager-demo/file-manager-demo.component";
 import { IndexComponent } from "./_components/index/index.component";
 
 @NgModule({
@@ -16,16 +18,19 @@ import { IndexComponent } from "./_components/index/index.component";
         ChatUiDemoComponent,
         FormsDemoComponent,
         IndexComponent,
+        FileManagerDemoComponent,
     ],
     imports: [
         BrowserModule,
         ChatUiModule,
         ReactiveFormsModule,
         FormsModule,
+        FileManagerModule,
         SchoolSquirrelFormsModule,
         RouterModule.forRoot([
             { path: "chat-ui", component: ChatUiDemoComponent },
             { path: "forms", component: FormsDemoComponent },
+            { path: "filemanager", component: FileManagerDemoComponent },
             { path: "", component: IndexComponent },
         ]),
     ],
